457a0f64ed226bd6a69df1f3f8cfb13f69889125
AkshayBaviskar07 <113607091+AkshayBaviskar07@users.noreply.github.com>
2023-06-17 00:35:34 +0530

    changes done

---
+++ Instahyre  Task-1/src/com/temprature/Main.java
@@ -0,0 +1,55 @@
+package com.temprature;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void celcToFarenheit(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-Enter Temperature (in celcius)-");
+        float temp = sc.nextFloat();
+        float result = (temp * 9 / 5) + 32;
+
+        System.out.println(result+" Farenheit");
+    }
+
+    public static void farenheitToCel(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-Enter Temperature (in farenheit)");
+        float temp = sc.nextFloat();
+        float result = (temp - 32) / 5*9;
+
+        System.out.println(result+" Celcius");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        System.out.println("---Welcome To Temperature Converter---");
+
+        do{
+            System.out.println("Choose temperature format you want to convert \n1.In Farenheit \n2.In Celcius");
+            int tempChoice = sc.nextInt();
+
+            switch(tempChoice){
+                case 1: celcToFarenheit();
+                        break;
+
+                case 2: farenheitToCel();
+                        break;
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
+
+            System.out.println("---Do you want to continue press(1) to continue (0) to exit---");
+            choice = sc.nextInt();
+
+            if(choice == 0){
+                System.out.println("Thank you for using this app.");
+            }
+        }while(choice != 0);
+    }
+}
